055dc00d8f28e47c8004fcd9ca7188705626a641
IlyaShcelkanov <l0stt.ttime@gmail.com>
2023-02-27 13:12:38 +0300

    initial commit

---
+++ OneDrive/Рабочий стол/c#hw6/41/Program.cs
@@ -0,0 +1,104 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+
+int EntNum(string text)
+{
+    System.Console.Write(text);
+    int result = int.Parse(Console.ReadLine()!); 
+    return result;
+}
+
+
+int[] EntArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = EntNum($"Введите {i + 1}й элемент: ");
+    }
+    return array;
+}
+
+
+
+void PrintArray(int[] entarray)
+{
+    for (int i = 0; i < entarray.Length; i++)
+    {
+        Console.Write($"{entarray[i]} ");
+    }
+}
+
+
+
+int CountPositive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+
+int lenght = EntNum("Введите кол-во символов: ");
+int[] array = EntArray(lenght);
+PrintArray(array);
+Console.WriteLine($" -> {CountPositive(array)}");
+
+
+
+
+// int CheckNumber(int a, int s, int d, int f, int g, int h)
+// {
+//     int count = 0;
+//     if (a > 0)
+//     {
+//         count++;
+//     }
+//     if (s > 0)
+//     {
+//         count++;
+//     }
+//     if (d > 0)
+//     {
+//         count++;
+//     }
+//     if (f > 0)
+//     {
+//         count++;
+//     }
+//     if (g > 0)
+//     {
+//         count++;
+//     }
+//     if (h > 0)
+//     {
+//         count++;
+//     }
+//     return count;
+// }
+
+
+
+
+// int ent1 = EntNum("ВВедите сторону 1: ");
+// int ent2 = EntNum("ВВедите сторону 2: ");
+// int ent3 = EntNum("ВВедите сторону 3: ");
+// int ent4 = EntNum("ВВедите сторону 4: ");
+// int ent5 = EntNum("ВВедите сторону 5: ");
+// int ent6 = EntNum("ВВедите сторону 6: ");
+
+// int res = CheckNumber(ent1, ent2, ent3, ent4, ent5, ent6);
+// Console.WriteLine(res);
---
+++ OneDrive/Рабочий стол/c#hw6/43/Program.cs
@@ -0,0 +1,63 @@
+﻿
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
+
+const int COEF = 0;
+const int CONST = 1;
+const int X = 0;
+const int Y = 1;
+const int LINE1 = 1;
+const int LINE2 = 2;
+
+double[] lineData1 = InputLineData(LINE1);
+double[] lineData2 = InputLineData(LINE2);
+
+if (ValidateLines(lineData1, lineData2))
+{
+    double[] coord = FindCoord(lineData1, lineData2);
+    Console.Write($"Точка пересечений уравнений y = {lineData1[COEF]} * x + {lineData2[COEF]} и y = {lineData2[COEF]} * x + {lineData2[COEF]}");
+    Console.WriteLine($" имеет координаты ({coord[X]}, {coord[Y]})");
+}
+
+double Prompt(string text)
+{
+    Console.Write(text);
+    double result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+double[] InputLineData(int numberOfLine)
+{
+    double[] lineData = new double[2];
+    lineData[COEF] = Prompt($"Введите коэффициент для {numberOfLine} прямой >");
+    lineData[CONST] = Prompt($"Введите константу для {numberOfLine} прямой >");
+    return lineData;
+}
+double[] FindCoord(double[] lineData1, double[] lineData2)
+{
+    double[] coord = new double[2];
+    coord[X] = ((lineData1[CONST] - lineData2[CONST]) / (lineData2[COEF] - lineData1[COEF]));
+    coord[Y] = lineData1[CONST] * coord[X] + lineData1[CONST];
+    return coord;
+}
+bool ValidateLines(double[] lineData1, double[] lineData2)
+{
+    if(lineData1[COEF] == lineData2[COEF])
+    {
+        if(lineData1[CONST] == lineData2[CONST])
+        {
+            Console.Write("Прямые совпадают");
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Прямые параллельны");
+            return false;
+        }
+    }
+    return true;
+}
